a9d440fa3d0f03df1d87ea9b63c27438a4e1d0b6
kjwamlex <Joonwoo@imac.unet.ca>
2016-03-18 17:49:00 -0700

    Initial Commit

---
+++ coolApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coolApp
+//
+//  Created by 김준우 on 2016. 3. 18..
+//  Copyright © 2016년 김준우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
